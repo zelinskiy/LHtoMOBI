52c235cb42b626bbb95c8ce8f7783711df82663c
Nick Yurchenko <nic.yurchenko@gmail.com>
2019-02-06 17:55:13 +0200

    Some fixes

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 # invitatorium lectionis laudes media vesperae completorium;
 
 now=`date +"%Y-%m-%d" -d "02/01/2019"`
-end=`date +"%Y-%m-%d" -d "02/10/2019"`
+end=`date +"%Y-%m-%d" -d "02/05/2019"`
 
 cat 'header.html' > test.html
 while [ "$now" != "$end" ] ;
@@ -12,9 +12,10 @@ do
     now=`date +"%Y-%m-%d" -d "$now + 1 day"`;
     echo "\n\n$now\n\n"
     echo "<h3>$now</h3>" >> test.html
-    for hour in invitatorium laudes vesperae completorium; do
+    for hour in invitatorium lectionis laudes media vesperae completorium; do
         echo "<h4>$hour</h4>" >> test.html
-        curl -d "hora=$hour&hour&date=$now&psalm_numbering=hebrew" \
+        curl -d "hora=$hour&hour&date=$now&input_date=$date&psalm_numbering=greek" \
+            -H "Content-Type: application/x-www-form-urlencoded" \
             -X POST http://www.liturgia-horarum.ru/phpordinarium.php \
             | sed -e 's/style="display:none;">/style="display:block;"><div class="h_title">или<\/div>/g' \
             >> test.html
