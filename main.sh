f1e70323aa7ed1df29f73054b20ce6f0e9969bfe
Nick Yurchenko <nic.yurchenko@gmail.com>
2019-02-08 16:28:34 +0200

    Added some

--- main.sh
+++ main.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 #now=`date +"%Y-%m-%d" -d "02/01/2019"`
-now=`date +"%Y-%m-%d"`
-end=`date +"%Y-%m-%d" -d $1`
+now=`date +"%Y-%m-%d" -d $1`
+end=`date +"%Y-%m-%d" -d $2`
 
 cat 'header.html' > test.html
 while [ "$now" != "$end" ] ;
-do
-    now=`date +"%Y-%m-%d" -d "$now + 1 day"`;
+do    
     echo "\n\n$now\n\n"
     echo "<h3>$now</h3>" >> test.html
     for hour in invitatorium lectionis laudes media vesperae completorium; do
@@ -15,13 +14,14 @@ do
         curl -d "hora=$hour&input_date=$now&psalm_numbering=greek" \
             -X POST http://www.liturgia-horarum.ru/phpordinarium.php \
             | sed -e 's/style="display:none;">/style="display:block;"><div class="h_title">или<\/div>/g' \
-            >> test.html
-
+            >> test.html    
     done
+    now=`date +"%Y-%m-%d" -d "$now + 1 day"`;
 done
 cat 'footer.html' >> test.html
 
 
 cd tmp
+# rm ../test.epub
 mv ../test.html Text/main.xhtml
 zip -r ../test.epub *
